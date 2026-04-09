54b54f551976698d24af8f2e87eacb14357fe40f
todoom1 <aa13976383@outlook.com>
2026-04-09 23:20:43 +0800

    更新 list.js

--- list.js
+++ list.js
@@ -62,3 +62,10 @@ if ('serviceWorker' in navigator) {
       console.log('SW registered');
     });
 }
+君临https://tfajn.hcjwj.com/app/register.php?site_id=2081&topId=3006236&selfPlanId=2578229
+新时代https://derear.fnbpsw.com/app/register.php?site_id=1059&topId=2189996&selfPlanId=1704529
+胜天国际https://lejva.tprsmi.com/app/register.php?site_id=1058&topId=3034522&selfPlanId=2331350
+28圈https://149.30.210.144:35384/#/link?allwin=Eciwj88759448&v=826
+1号https://gfvgx.jgqvji.com/app/register.php?site_id=800&pt=378F0836-F66C-EEB3-7EAC-93CA8488E07F
+超凡https://pkajim.hfjcl.com/app/register.php?site_id=1032&pt=46AD1D17-79C4-98BC-C25A-AA725CD99B68
+https://lncqc.jgqvji.com/app/register.php?site_id=1050&topId=6721763483224422&selfPlanId=132103
